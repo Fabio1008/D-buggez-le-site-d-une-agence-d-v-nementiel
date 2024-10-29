878cf03d8613a352cf419b170eda07ae4ad00c9c
Fabrice <amonkefabrice@gmail.com>
2024-10-29 04:31:19 +0100

    liaison de valeur et gestion de modif

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -7,7 +7,7 @@ export const FIELD_TYPES = {
   TEXTAREA: 2,
 };
 
-const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
+const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder, value, onChange}) => {
   let component;
   switch (type) {
     case FIELD_TYPES.INPUT_TEXT:
@@ -16,12 +16,18 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
           type="text"
           name={name}
           placeholder={placeholder}
+          value={value}         // Liaison de la valeur
+          onChange={onChange}    // Gestion de la modification
           data-testid="field-testid"
         />
       );
       break;
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid" />;
+      component = <textarea name={name}
+      placeholder={placeholder}
+          value={value}         // Liaison de la valeur
+          onChange={onChange}    // Gestion de la modification 
+      data-testid="field-testid" />;
       break;
     default:
       component = (
@@ -29,6 +35,8 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
           type="text"
           name={name}
           placeholder={placeholder}
+          value={value}
+          onChange={onChange}
           data-testid="field-testid"
         />
       );
@@ -46,12 +54,16 @@ Field.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string,
+  value: PropTypes.string,          // Validation du type `value`
+  onChange: PropTypes.func,         // Validation du type `onChange`
 };
  Field.defaultProps = {
    label: "",
    placeholder: "",
    type: FIELD_TYPES.INPUT_TEXT,
    name: "field-name",
+   value: "",                         // Valeur par défaut vide
+   onChange: () => {},                // Fonction par défaut vide
  }
 
 export default Field;
